21dc7b9459547beef7a6b0d972b10f5851c343ec
jhonsfranky17 <franklinjettyjohnson6@gmail.com>
2024-07-08 20:27:35 +0530

    Style: Our services

--- scripts/hero.js
+++ scripts/hero.js
@@ -1,8 +1,8 @@
-const services = ["web app!","design!","website!"];
+const services = ["web app","design","website"];
 const serviceElement = document.getElementById("service");
 let i = 0;
 setInterval(()=>{
-        serviceElement.innerHTML = services[i];
+        serviceElement.innerHTML = services[i] + "!";
         if(++i === services.length)
             i=0;
     },2000);
